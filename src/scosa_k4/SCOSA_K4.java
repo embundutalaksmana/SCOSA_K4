41bce3cd821220a8a24aac5600f6672f6e1878c6
Embun Duta Laksmana <embun20ti@mahasiswa.pcr.ac.id>
2021-07-03 20:27:56 +0700

    Scosa_main

---
+++ src/scosa_k4/SCOSA_K4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scosa_k4;
+
+/**
+ *
+ * @author Embun Duta Laksmana
+ */
+public class SCOSA_K4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
