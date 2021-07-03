87df9661082224bc1329dd4e78139bcf6edfc1d9
Embun Duta Laksmana <embun20ti@mahasiswa.pcr.ac.id>
2021-07-03 20:41:34 +0700

    Scosa_konfigurasi

--- src/konfigurasi/CONFIG.java
+++ src/konfigurasi/CONFIG.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package konfigurasi;
 
-/**
- *
- * @author Embun Duta Laksmana
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class CONFIG {
-    
+    private static Connection MySQLConfig;
+    public static Connection configDB()throws SQLException {
+        try{
+            String url = "jdbc:mysql://localhost/database_scosa";
+            String user = "root";
+            String pass = "";
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            MySQLConfig = DriverManager.getConnection(url, user, pass);
+        }catch(SQLException e){
+            System.out.println("Gagal Mengoneksikan Ke Database" + e.getMessage());
+        }
+        return MySQLConfig;   
+    }   
 }
