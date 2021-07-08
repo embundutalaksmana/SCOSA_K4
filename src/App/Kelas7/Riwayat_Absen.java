46a92d6fb631bcb125d9cb5b88d1904233dd4115
Embun Duta Laksmana <embun20ti@mahasiswa.pcr.ac.id>
2021-07-08 22:09:00 +0700

    Revisi2

--- src/App/Kelas7/Riwayat_Absen.java
+++ src/App/Kelas7/Riwayat_Absen.java
@@ -30,7 +30,7 @@ public class Riwayat_Absen extends javax.swing.JFrame {
         NIS_f.setEditable(true);
         NIS_f.setText(null);
         Nama_f.setText(null);
-        Kelas_f.setText(null);
+        
     }
     private void print_data(){
         DefaultTableModel model = new DefaultTableModel();
@@ -83,11 +83,8 @@ public class Riwayat_Absen extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         Table_siswa = new javax.swing.JTable();
         Cari_tanggal = new javax.swing.JButton();
-        Tahun3 = new javax.swing.JLabel();
-        Kelas_f = new javax.swing.JTextField();
         Cari_NIS = new javax.swing.JButton();
         Cari_Nama = new javax.swing.JButton();
-        Cari_Kelas = new javax.swing.JButton();
         Refresh = new javax.swing.JButton();
         Kembali = new javax.swing.JButton();
         tnggl1 = new com.toedter.calendar.JDateChooser();
@@ -138,14 +135,6 @@ public class Riwayat_Absen extends javax.swing.JFrame {
             }
         });
 
-        Tahun3.setText("Kelas: ");
-
-        Kelas_f.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                Kelas_fActionPerformed(evt);
-            }
-        });
-
         Cari_NIS.setText("Cari");
         Cari_NIS.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -157,13 +146,6 @@ public class Riwayat_Absen extends javax.swing.JFrame {
         Cari_Nama.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 Cari_NamaActionPerformed(evt);
-            }
-        });
-
-        Cari_Kelas.setText("Cari");
-        Cari_Kelas.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                Cari_KelasActionPerformed(evt);
             }
         });
 
@@ -212,14 +194,9 @@ public class Riwayat_Absen extends javax.swing.JFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(Tahun2)
                                 .addGap(21, 21, 21)
-                                .addComponent(NIS_f, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(Tahun3)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(Kelas_f, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addComponent(NIS_f, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)))
                         .addGap(39, 39, 39)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(Cari_Kelas)
                             .addComponent(Cari_Nama)
                             .addComponent(Cari_NIS))))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -259,12 +236,7 @@ public class Riwayat_Absen extends javax.swing.JFrame {
                     .addComponent(Tahun1)
                     .addComponent(Nama_f, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(Cari_Nama))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(Tahun3)
-                    .addComponent(Kelas_f, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(Cari_Kelas))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 71, Short.MAX_VALUE)
                 .addComponent(Refresh)
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -308,10 +280,6 @@ public class Riwayat_Absen extends javax.swing.JFrame {
             System.out.println("Error : " + e.getMessage());
         }
     }//GEN-LAST:event_Cari_tanggalActionPerformed
-
-    private void Kelas_fActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Kelas_fActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_Kelas_fActionPerformed
 
     private void Cari_NISActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Cari_NISActionPerformed
        DefaultTableModel model = new DefaultTableModel();
@@ -366,33 +334,6 @@ public class Riwayat_Absen extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_Cari_NamaActionPerformed
 
-    private void Cari_KelasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Cari_KelasActionPerformed
-        DefaultTableModel model = new DefaultTableModel();
-        model.addColumn("No.");
-        model.addColumn("Tanggal");
-        model.addColumn("NIS");
-        model.addColumn("Nama");
-        model.addColumn("Kelas");
-        model.addColumn("Keterangan");
-         try{
-            int no = 1;
-            String sql = "SELECT TANGGAL,NIS,NAMA,KELAS,KETERANGAN FROM presensi WHERE Kelas ='"+Kelas_f.getText()+"'";
-            java.sql.Connection conn = (Connection)CONFIG.configDB();
-            java.sql.Statement stm = conn.createStatement();
-            java.sql.ResultSet res = stm.executeQuery(sql);
-            
-            while(res.next()) {
-               model.addRow(new Object[] {no++ , res.getString(1) , res.getString(2),
-               res.getString(3), res.getString(4), res.getString(5)});
-            } 
-              
-            Table_siswa.setModel(model);
-            
-        } catch(SQLException e) {
-            System.out.println("Error : " + e.getMessage());
-        }
-    }//GEN-LAST:event_Cari_KelasActionPerformed
-
     private void RefreshActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RefreshActionPerformed
       print_data();      
     }//GEN-LAST:event_RefreshActionPerformed
@@ -416,11 +357,9 @@ public class Riwayat_Absen extends javax.swing.JFrame {
     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton Cari_Kelas;
     private javax.swing.JButton Cari_NIS;
     private javax.swing.JButton Cari_Nama;
     private javax.swing.JButton Cari_tanggal;
-    private javax.swing.JTextField Kelas_f;
     private javax.swing.JButton Kembali;
     private javax.swing.JTextField NIS_f;
     private javax.swing.JTextField Nama_f;
@@ -428,7 +367,6 @@ public class Riwayat_Absen extends javax.swing.JFrame {
     private javax.swing.JTable Table_siswa;
     private javax.swing.JLabel Tahun1;
     private javax.swing.JLabel Tahun2;
-    private javax.swing.JLabel Tahun3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
